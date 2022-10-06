64429e51995d648b959882630ae2628886b86dd0
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-10-06 19:07:53 +0300

    initial commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,12 +4,7 @@ import {useEffect, useState} from 'react'
 function Search({contact}) {
   const [contacts, setContacts] = useState([])
 
-  useEffect(() => {
-    fetch('https://contact-list-server-app.herokuapp.com/Contacts')
-    .then(res => res.json())
-    .then(data =>console.log(data))
-  }, [])
-  
+
   return (
     <div className='search'>
         <input id="searchInput" type="text" placeholder='Search for a contact...'/>
