bd05d7ca30ed4c5c7015bc7605f850d6e66fc2dc
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-10-07 13:17:50 +0300

    link cancel button to Home page

--- src/components/NewContact.js
+++ src/components/NewContact.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function NewContact() {
   return (
     <form id="form">
-      <input type="text" name="name" placeholder='Name'/>
-      <input type="tel" name="phone" placeholder='Phone'/>
-      <input type="text" name="email" placeholder='Email'/>
-      <input type="text" name="company" placeholder='Company'/>
-      <input type="text" name="location" placeholder='Location'/>
+      <input type="text" name="name" placeholder='Name'/><br/>
+      <input type="tel" name="phone" placeholder='Phone'/><br/>
+      <input type="text" name="email" placeholder='Email'/><br/>
+      <input type="text" name="company" placeholder='Company'/><br/>
+      <input type="text" name="location" placeholder='Location'/><br/>
       <input type="text" name="description" placeholder='Description'/>
       <button type="submit">Save</button>
+      <Link className='nav-links' to='/home'>
+         <button type="submit">Cancel</button>
+      </Link>
     </form>
 )
   }
