8ed085fe2f4acc68b64b326d18eede75f52e265d
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-10-07 15:11:27 +0300

    get data from form

--- src/components/NewContact.js
+++ src/components/NewContact.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import {useState} from 'react'
 import { Link } from 'react-router-dom';
 
 function NewContact() {
+  const [name, setName] = useState("")
+  const [phone, setPhone] = useState("")
+  const [email, setEmail] = useState("")
+  const [company, setCompany] = useState("")
+  const [location, setLocation] = useState("")
+  const [description, setDescription] = useState("")
+  // console.log(name, phone)
+
+  function handleSubmit(e){
+    e.preventDefault()
+  }
   return (
     <form id="form">
-      <input type="text" name="name" placeholder='Name'/><br/>
-      <input type="tel" name="phone" placeholder='Phone'/><br/>
-      <input type="text" name="email" placeholder='Email'/><br/>
-      <input type="text" name="company" placeholder='Company'/><br/>
-      <input type="text" name="location" placeholder='Location'/><br/>
-      <input type="text" name="description" placeholder='Description'/>
-      <button type="submit">Save</button>
+      <input type="text" name="name" placeholder='Name' value={name} onChange={e => setName(e.target.value)}/><br/>
+      <input type="tel" name="phone" placeholder='Phone' value={phone} onChange={e => setPhone(e.target.value)}/><br/>
+      <input type="text" name="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/><br/>
+      <input type="text" name="company" placeholder='Company' value={company} onChange={e => setCompany(e.target.value)}/><br/>
+      <input type="text" name="location" placeholder='Location' value={location} onChange={e => setLocation(e.target.value)}/><br/>
+      <input type="text" name="description" placeholder='Description' value={description} onChange={e => setDescription(e.target.value)}/>
+      <button type="submit" onClick={handleSubmit}>Save</button>
       <Link className='nav-links' to='/home'>
          <button type="submit">Cancel</button>
       </Link>
